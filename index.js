e504f1662f42d9ab3e8765b62a9171e606f97207
Jairo Odeli Junior <odelijairo@gmail.com>
2018-07-14 18:47:45 -0300

    code from aws

---
+++ index.js
@@ -0,0 +1,27 @@
+const sendEmail = require('./send-mail')
+
+exports.handler = async (event, context, callback) => {
+  const response = {
+    "statusCode": 200,
+    "headers": {
+      "Access-Control-Allow-Origin": "*"
+    },
+    "isBase64Encoded": false
+  };
+  try {
+    const requestBody = JSON.parse(event.body);
+    if (requestBody && (requestBody.mail || requestBody.phone)) {
+      await sendEmail(requestBody);
+      response.body = 'success';
+      callback(null, response);
+    } else {
+      response.statusCode = 500;
+      response.body = 'Invalid payload: ' + JSON.stringify(event);
+      callback(null, response);
+    }
+  } catch (e) {
+    response.statusCode = 500;
+    response.body = e.toString();
+    return callback(null, response)
+  }
+};
